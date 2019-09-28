d86bd1cc011f61e0f38088a0d69de00eff3b2ee6
Yan Shevtsov <fatwebdev@gmail.com>
2019-09-28 21:20:55 +0300

    add User class

---
+++ user.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class User < Player
+  def turn_options
+    {
+      pass: '1 - pass',
+      take_card: '2 - take card',
+      show_hand: '3 - show hand'
+    }
+  end
+
+  def render_info
+    puts "You have #{amount} money, cards on hand: #{show_cards} with total score: #{score}"
+  end
+
+  def turn
+    render_info
+    choiced = choice
+    puts
+    turn_options.keys[choiced - 1]
+  end
+
+  def choice
+    puts turn_options.values
+    print 'Select option... '
+    gets.chomp.to_i
+  end
+end
